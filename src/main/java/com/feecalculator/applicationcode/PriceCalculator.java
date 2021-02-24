9eeaaf1145f7a6bd99c3745ead766013ca0a3e2c
Schlueter <kai.udo.schlueter@sap.com>
2021-02-24 23:07:18 +0100

    new changes

--- src/main/java/com/feecalculator/applicationcode/PriceCalculator.java
+++ src/main/java/com/feecalculator/applicationcode/PriceCalculator.java
@@ -5,4 +5,6 @@ import com.feecalculator.domaincode.Transaction;
 public class PriceCalculator {
 
     private Transaction transaction;
+
+    //Restbetrag: (Restbetrag + Transaktionsgebühr) + (Restbetrag + Transaktionsgebühr) * Transaktionsrate
 }
--- src/main/java/com/feecalculator/applicationcode/TansactionFeeCalculator.java
+++ src/main/java/com/feecalculator/applicationcode/TansactionFeeCalculator.java
@@ -2,6 +2,7 @@ package com.feecalculator.applicationcode;
 
 import com.feecalculator.domaincode.Amount;
 import com.feecalculator.domaincode.Transaction;
+import com.feecalculator.domaincode.TransactionType;
 
 public class TansactionFeeCalculator {
 
@@ -11,7 +12,39 @@ public class TansactionFeeCalculator {
         this.transaction = transaction;
     }
 
-    private Amount calculateTransactionFee(){
+    private Amount calculateTransactionFee(Transaction transaction) {
+        if (transaction.getType() == TransactionType.PAYPAL_PERSONAL_TRANSACTION) {
+            double transactionfee = 0.00 * transaction.getAmount().getAmount() + 0.0;
+            return new Amount(transactionfee, transaction.getAmount().getCurrency());
+        }
+        if (transaction.getType() == TransactionType.PAYPAL_DONATION) {
+            double transactionfee = 0.015 * transaction.getAmount().getAmount() + 0.35;
+            return new Amount(transactionfee, transaction.getAmount().getCurrency());
+        }
+        if (transaction.getType() == TransactionType.PAYPAL_SERVICE_PAYMENT) {
+            double transactionfee = 0.0249 * transaction.getAmount().getAmount() + 0.35;
+            return new Amount(transactionfee, transaction.getAmount().getCurrency());
+        }
+        if (transaction.getType() == TransactionType.PAYPAL_MICRO_PAYMENT) {
+            double transactionfee = 0.1 * transaction.getAmount().getAmount() + 0.1;
+            return new Amount(transactionfee, transaction.getAmount().getCurrency());
+        }
+        if (transaction.getType() == TransactionType.PAYPAL_SELLER_CONDITION_LOW_VOLUME) {
+            double transactionfee = 0.0219 * transaction.getAmount().getAmount() + 0.35;
+            return new Amount(transactionfee, transaction.getAmount().getCurrency());
+        }
+        if (transaction.getType() == TransactionType.PAYPAL_SELLER_CONDITION_MEDIUM_VOLUME) {
+            double transactionfee = 0.0199 * transaction.getAmount().getAmount() + 0.35;
+            return new Amount(transactionfee, transaction.getAmount().getCurrency());
+        }
+        if (transaction.getType() == TransactionType.PAYPAL_SELLER_CONDITION_HIGH_VOLUME) {
+            double transactionfee = 0.0179 * transaction.getAmount().getAmount() + 0.35;
+            return new Amount(transactionfee, transaction.getAmount().getCurrency());
+        }
+        if (transaction.getType() == TransactionType.PAYPAL_SELLER_CONDITION_VERY_HIGH_VOLUME) {
+            double transactionfee = 0.0149 * transaction.getAmount().getAmount() + 0.35;
+            return new Amount(transactionfee, transaction.getAmount().getCurrency());
+        }
         return null;
     }
 }
