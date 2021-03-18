6b0093f3ed53b3449b9ad033c36b6af29f055a41
Schlueter <kai.udo.schlueter@sap.com>
2021-03-18 23:45:02 +0100

    refactor calculateFeeFor method

--- src/main/java/com/feecalculator/applicationcode/TansactionFeeCalculator.java
+++ src/main/java/com/feecalculator/applicationcode/TansactionFeeCalculator.java
@@ -12,119 +12,49 @@ public class TansactionFeeCalculator {
 
     //Sichbarkeitsstufe 2: Domain Driven Design
 
-    public static Amount calculateFeeFor(Transaction transaction) {
+    public Amount calculateFeeFor(Transaction transaction) {
 
         if (transaction.getTransactionVolume()
                 .getCurrency() == Currency.getInstance("EUR")) {
             if (transaction.getTransactionType() == TransactionType.PAYPAL_PERSONAL_TRANSACTION) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
+                return createAmountFor(transaction);
             }
             if (transaction.getTransactionType() == TransactionType.PAYPAL_DONATION) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
-            }
-            if (transaction.getTransactionType() == TransactionType.PAYPAL_MICRO_PAYMENT) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
-            }
-            if (transaction.getTransactionType() == TransactionType.PAYPAL_MICRO_PAYMENT) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
-            }
-            if (transaction.getTransactionType() == TransactionType.PAYPAL_MICRO_PAYMENT) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
-            }
-            if (transaction.getTransactionType() == TransactionType.PAYPAL_MICRO_PAYMENT) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
-            }
-            if (transaction.getTransactionType() == TransactionType.PAYPAL_MICRO_PAYMENT) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
+                return createAmountFor(transaction);
             }
             if (transaction.getTransactionType() == TransactionType.PAYPAL_SELLER_CONDITION_LOW_VOLUME) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
+                return createAmountFor(transaction);
             }
             if (transaction.getTransactionType() == TransactionType.PAYPAL_SELLER_CONDITION_MEDIUM_VOLUME) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
+                return createAmountFor(transaction);
             }
             if (transaction.getTransactionType() == TransactionType.PAYPAL_SELLER_CONDITION_HIGH_VOLUME) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
+                return createAmountFor(transaction);
             }
             if (transaction.getTransactionType() == TransactionType.PAYPAL_SELLER_CONDITION_VERY_HIGH_VOLUME) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
+                return createAmountFor(transaction);
             }
             if (transaction.getTransactionType() == TransactionType.VISA_CONCARDIS) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
+                return createAmountFor(transaction);
             }
             if (transaction.getTransactionType() == TransactionType.VISA_WALLEE) {
-                double transactionFee = transaction.getTransactionType()
-                        .getFixedFee()
-                        .getValue() + transaction.getTransactionVolume()
-                        .getValue() * transaction.getTransactionType()
-                        .getVariableFee();
-                return new Amount(transactionFee, Currency.getInstance("EUR"));
+                return createAmountFor(transaction);
             }
             throw new UnkownTransactionType("The transaction type " + transaction.getTransactionType() + " is unkown to the system.");
         } else {
             throw new NotSupportedCurrencyException("The Currency " + transaction.getTransactionVolume()
                     .getCurrency() + " is not supported.");
         }
+    }
+
+    private Amount createAmountFor(Transaction transaction) {
+        return new Amount(calculateFeeWith(transaction.getTransactionVolume(), transaction.getTransactionType()), transaction.getTransactionVolume()
+                .getCurrency());
+    }
+
+    private double calculateFeeWith(Amount transactionVolume, TransactionType transactionType) {
+        return transactionType.getFixedFee()
+                .getValue() + transactionVolume.getValue() * transactionType.getVariableFee();
     }
 
 }
--- src/main/java/com/feecalculator/domaincode/TransactionType.java
+++ src/main/java/com/feecalculator/domaincode/TransactionType.java
@@ -2,10 +2,15 @@ package com.feecalculator.domaincode;
 
 public enum TransactionType {
 
-    PAYPAL_PERSONAL_TRANSACTION(ConfigurationProperties.FRIENDS_FEE, 0), PAYPAL_DONATION(ConfigurationProperties.SELLER_FEE, 0.015), PAYPAL_SELLER_CONDITION_LOW_VOLUME(
-            ConfigurationProperties.SELLER_FEE, 0.0219), PAYPAL_SELLER_CONDITION_MEDIUM_VOLUME(ConfigurationProperties.SELLER_FEE, 0.0199), PAYPAL_SELLER_CONDITION_HIGH_VOLUME(
-            ConfigurationProperties.SELLER_FEE, 0.0179), PAYPAL_SELLER_CONDITION_VERY_HIGH_VOLUME(ConfigurationProperties.SELLER_FEE, 0.0149), PAYPAL_MICRO_PAYMENT(
-            ConfigurationProperties.MICROPAYMENT_FEE, 0.1), VISA_WALLEE(ConfigurationProperties.WALLEE_FEE, 0.014), VISA_CONCARDIS(ConfigurationProperties.CONCARDIS_FEE, 0.0099);
+    PAYPAL_PERSONAL_TRANSACTION(ConfigurationProperties.FRIENDS_FEE, 0),
+    PAYPAL_DONATION(ConfigurationProperties.SELLER_FEE, 0.015),
+    PAYPAL_SELLER_CONDITION_LOW_VOLUME(ConfigurationProperties.SELLER_FEE, 0.0219),
+    PAYPAL_SELLER_CONDITION_MEDIUM_VOLUME(ConfigurationProperties.SELLER_FEE, 0.0199),
+    PAYPAL_SELLER_CONDITION_HIGH_VOLUME(ConfigurationProperties.SELLER_FEE, 0.0179),
+    PAYPAL_SELLER_CONDITION_VERY_HIGH_VOLUME(ConfigurationProperties.SELLER_FEE, 0.0149),
+    PAYPAL_MICRO_PAYMENT(ConfigurationProperties.MICROPAYMENT_FEE, 0.1),
+    VISA_WALLEE(ConfigurationProperties.WALLEE_FEE, 0.014),
+    VISA_CONCARDIS(ConfigurationProperties.CONCARDIS_FEE, 0.0099);
 
     private Amount fixedFee;
     private double variableFee;
